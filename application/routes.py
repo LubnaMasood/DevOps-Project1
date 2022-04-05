438d643f404b7f5282c87201e1049485f6e59479
Lubna Masood <lubnamasood40@gmail.com>
2022-04-05 13:03:14 +0000

    LM: template changes

--- application/routes.py
+++ application/routes.py
@@ -21,4 +21,25 @@ def placeorder():
     new_order = Customer_order(name="Order Placed!")
     db.session.add(new_order)
     db.session.commit()
-    return "Added new customer order to the database!"
+    return "Added your order into the system!"
+
+@app.route('/vieworder', methods = ['GET', 'POST'])
+def vieworder():
+    view_order = Customer_order(name="There are no orders to view!")
+    db.session.add(view_order)
+    db.session.commit()
+    return "Place an order to view your orders!"
+
+@app.route('/update', methods = ['GET', 'POST'])
+def update():
+    update = Customer(name="Update your information!")
+    db.session.add(update)
+    db.session.commit()
+    return "Your information has been updated!"
+
+@app.route('/delete', methods = ['GET', 'POST'])
+def delete():
+    delete = Customer(name="Delete your information!")
+    db.session.add(delete)
+    db.session.commit()
+    return "Your information has been deleted!"
